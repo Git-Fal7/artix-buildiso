2ef0bb3bb262f91aaaad2ea0fb10cf19f3912724
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-17 14:31:42 +0300

    Update putconf.sh

--- zorin-lite-artix/putconf.sh
+++ zorin-lite-artix/putconf.sh
@@ -10,6 +10,7 @@ cp /etc/sudoers /var/lib/artools/buildiso/lxde/artix/rootfs/etc/
 rm -rf /var/lib/artools/buildiso/lxde/artix/rootfs/usr/bin/lxclipboard
 
 echo copying scripts to build aur packages
+cp ~/sudoers /var/lib/artools/buildiso/lxde/artix/rootfs/etc
 cp ~/chrootroot.sh /var/lib/artools/buildiso/lxde/artix/rootfs/usr
 cp ~/chrootuser.sh /var/lib/artools/buildiso/lxde/artix/rootfs/usr
 
