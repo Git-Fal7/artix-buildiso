e5b90a413645b0c985880f46b3c33ba4d837bf31
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-17 14:25:09 +0300

    Update chrootuser.sh

--- zorin-lite-artix/chrootuser.sh
+++ zorin-lite-artix/chrootuser.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-echo now we are getting the pkg of yay
-git clone https://aur.archlinux.org/yay
-
-echo now we cd onto yay
-cd ~/yay
-
-echo now we make the yay pkg
-makepkg -si
+echo nows using my script to install zorin stuff
+git clone https://github.com/git-fal7/zorin-os-lite-arch
+cd zorin-os-lite-arch/
+chmod +x ./install.sh
+./install.sh -nosystemd
 
 echo after that, we install opendoas-sudo
 yay -S opendoas-sudo
-
-echo nows using my script to install zorin stuff
-git clone https://github.com/git-fal7/zorin-os-lite-arch
-cd zorin-os-lite-arch
-chmod +x ./install.sh
-./install.sh -artix
 
 echo removing orphan packages
 sudo pacman -Rsn $(pacman -Qdtq)
